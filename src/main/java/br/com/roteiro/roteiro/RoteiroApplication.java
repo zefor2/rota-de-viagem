a70da120db3e0c77ef21cef6f2fa4473124d670c
zefor2 <zefor2@gmail.com>
2019-04-30 00:14:17 -0300

    aplicacao

---
+++ src/main/java/br/com/roteiro/roteiro/RoteiroApplication.java
@@ -0,0 +1,13 @@
+package br.com.roteiro.roteiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoteiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoteiroApplication.class, args);
+	}
+
+}
